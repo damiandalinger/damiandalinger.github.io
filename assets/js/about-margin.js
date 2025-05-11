2e8e36e5678e6b120c56341d5a83783cc894bd1e
Damian <130753542+damiandalinger@users.noreply.github.com>
2025-05-11 16:18:50 +0200

    Fix the button & about-wrapper issue

---
+++ assets/js/about-margin.js
@@ -0,0 +1,3 @@
+	const heroHeight = document.querySelector('.hero-section').offsetHeight;
+	const overlap = heroHeight * 0.15;
+	document.querySelector('.about-wrapper').style.marginTop = `-${overlap}px`;
